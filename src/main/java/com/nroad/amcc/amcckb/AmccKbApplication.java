7b33c51e6212aa0db83b64d1c2ccfb44fffd702a
yaozhf <yaozhf@xinli.com.cn>
2018-02-08 19:39:01 +0800

    init

---
+++ src/main/java/com/nroad/amcc/amcckb/AmccKbApplication.java
@@ -0,0 +1,12 @@
+package com.nroad.amcc.amcckb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmccKbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmccKbApplication.class, args);
+	}
+}
