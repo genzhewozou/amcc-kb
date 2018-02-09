193bb6a5c3e480dac1678d06401caf537be8e418
yaozhf <yaozhf@xinli.com.cn>
2018-02-09 13:15:49 +0800

    init pro

--- src/main/java/com/nroad/amcc/AmccKbApplication.java
+++ src/main/java/com/nroad/amcc/AmccKbApplication.java
@@ -1,9 +1,12 @@
-package com.nroad.amcc.amcckb;
+package com.nroad.amcc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class AmccKbApplication {
 
 	public static void main(String[] args) {
